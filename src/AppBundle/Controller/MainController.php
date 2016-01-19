e393e72a5ef72b1512a52dc2e0660481713ab971
zewwwid <i@zewwwid.com>
2016-01-19 18:15:53 +0500

    Changed MainController to redirect to page documentation

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -9,8 +9,6 @@ class MainController extends Controller
 {
     public function indexAction(Request $request)
     {
-        return $this->render('AppBundle:Main:index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->redirectToRoute('nelmio_api_doc_index');
     }
 }
